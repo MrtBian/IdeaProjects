8b66a08caf8b7834352543bef14d5771d6c94831
MrtBian <2539070250@qq.com>
2017-11-22 22:23:26 +0800

    [TEST]

--- AGB_Test/src/main/MainClass.java
+++ AGB_Test/src/main/MainClass.java
@@ -8,6 +8,43 @@ public class MainClass {
     private double [] timeStamp;
     private double [] rssi;
     private double [] phase;
+    double THRESHOLD_RSSI_MAX_DIFF_TO_PEAK = 10;
+
+
+    private void processData() {
+        int num = phase.length;
+        double max = -70;
+        int maxIndex = 0;
+        for (int i = 0; i < num; i++) {
+            max = max > rssi[i] ? max : rssi[i];
+            maxIndex = max > rssi[i] ? maxIndex : i;
+        }
+
+        int startI = maxIndex - 1;
+        for(;startI >= 0 && max - rssi[startI] <= THRESHOLD_RSSI_MAX_DIFF_TO_PEAK;startI--);
+        System.out.println("startI: " + startI);
+
+        if(startI > 0) {
+            int newLen = num - startI;
+            double [] newTime = new double[newLen];
+            double [] newRssi = new double[newLen];
+            double [] newPhase = new double[newLen];
+            for(int j = 0, i = startI;j < newLen;i++, j++) {
+                newTime[j] = timeStamp[i];
+                newRssi[j] = rssi[i];
+                newPhase[j] = phase[i];
+            }
+
+            timeStamp = new double[newLen];
+            rssi = new double[newLen];
+            phase = new double[newLen];
+            for(int i = 0;i < newLen;i++) {
+                timeStamp[i] = newTime[i];
+                rssi[i] = newRssi[i];
+                phase[i] = newPhase[i];
+            }
+        }
+    }
 
     private void processPhase() {
         int len = phase.length;
@@ -33,9 +70,13 @@ public class MainClass {
         }
 
         if(leftMaxIndex != rightMaxIndex) {
-            int newLen = len - rightMaxIndex + leftMaxIndex;
+            final int THRESHOLD1 = 5;
+            leftMaxIndex = Math.max(0, leftMaxIndex - THRESHOLD1);
+            rightMaxIndex = Math.min(len - 1, rightMaxIndex + THRESHOLD1);
+            System.out.println("left: " + leftMaxIndex + "\nright: " + rightMaxIndex);
+            int newLen = len - rightMaxIndex + leftMaxIndex - 1;
             double [] newPhase = new double[newLen];
-            double [] newTime =new double[newLen];
+            double [] newTime = new double[newLen];
             int i = 0;
             for(;i < leftMaxIndex;i++) {
                 newPhase[i] = phase[i];
@@ -50,6 +91,37 @@ public class MainClass {
             for(int j = 0;j < newLen;j++) {
                 phase[j] = newPhase[j];
                 timeStamp[j] = newTime[j];
+            }
+        }
+    }
+    /**
+     * 消除相位跳变
+     */
+    private void eliminatePhaseJumps() {
+        int len = phase.length;
+        for(int i = 1;i < len;i++) {
+            double tmp = Math.abs(phase[i - 1] - phase[i]);
+            if(tmp > 2 && tmp < 5) {
+                if(phase[i - 1] > phase[i]) {
+                    for(int j = i;j < len;j++) {
+                        phase[j] += Math.PI;
+                    }
+                } else {
+                    for(int j = i;j < len;j++) {
+                        phase[j] -= Math.PI;
+                    }
+                }
+            }
+            if(tmp > 5) {
+                if(phase[i - 1] > phase[i]) {
+                    for(int j = i;j < len;j++) {
+                        phase[j] += 2 * Math.PI;
+                    }
+                } else {
+                    for(int j = i;j < len;j++) {
+                        phase[j] -= 2 * Math.PI;
+                    }
+                }
             }
         }
     }
@@ -69,8 +141,11 @@ public class MainClass {
             mainClass.rssi[i] = info.getRSSI();
             mainClass.phase[i] = info.getPhase();
         }
+        mainClass.eliminatePhaseJumps();
+        mainClass.processData();
         mainClass.processPhase();
-        //double a[] = mypolyfit.PolyFit(xTime, yRssi, num, 2);
+        num = mainClass.phase.length;
+//        double a[] = mypolyfit.PolyFit(mainClass.timeStamp, mainClass.phase, num, 3);
         LeastSquareMethod leastSquareMethod = new LeastSquareMethod(mainClass.timeStamp, mainClass.phase, 3);
         double a[] = leastSquareMethod.getCoefficient();
             /*if(a[2]>0)
