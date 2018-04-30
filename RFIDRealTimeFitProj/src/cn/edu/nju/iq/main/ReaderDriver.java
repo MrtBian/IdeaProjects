ad15ead6bb3830d8c262187b011686732d778486
Wing Bian <2539070250@qq.com>
2018-04-30 19:41:40 +0800

    1

--- RFIDRealTimeFitProj/src/cn/edu/nju/iq/main/ReaderDriver.java
+++
@@ -1,9 +0,0 @@
-package cn.edu.nju.iq.main;
-
-import cn.edu.nju.iq.reader.Reader;
-
-public class ReaderDriver {
-    public static void main(String [] args) {
-        new Reader().reading();
-    }
-}
